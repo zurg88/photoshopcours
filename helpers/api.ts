b881f6c21c36d021c7774b863f13bed64115d6fb
Sergey Gnatenko <gnatsenko@gmail.com>
2022-02-03 21:47:51 +0300

    fix failed request

--- helpers/api.ts
+++ helpers/api.ts
@@ -1,12 +1,12 @@
 export const API = {
 	topPage: {
-		find: '//courses-top.ru' + '/api/top-page/find',
-		byAlias: '//courses-top.ru' + '/api/top-page/byAlias/'
+		find: 'https://courses-top.ru' + '/api/top-page/find',
+		byAlias: 'https://courses-top.ru' + '/api/top-page/byAlias/'
 	},
 	product: {
-		find: '//courses-top.ru' + '/api/product/find'
+		find: 'https://courses-top.ru' + '/api/product/find'
 	},
 	review: {
-		createDemo: '//courses-top.ru' + '/api/review/create-demo'
+		createDemo: 'https://courses-top.ru' + '/api/review/create-demo'
 	}
 }
